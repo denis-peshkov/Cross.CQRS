e5f92893916aa32097df4484f285dc5d81b4dcc4
Denis Peshkov <denis.peshkov@outlook.com>
2025-03-13 16:10:28 +0200

    Release/add no stop watch measurement attribute (#16)
    * Make ObjectExtensions.GetObjectSize() safe for some cases.

--- Cross.CQRS/Extensions/ObjectExtensions.cs
+++ Cross.CQRS/Extensions/ObjectExtensions.cs
@@ -4,8 +4,24 @@ public static class ObjectExtensions
 {
     public static int GetObjectSize(this object obj)
     {
-        var jsonString = JsonSerializer.Serialize(obj);
-        var array = Encoding.UTF8.GetBytes(jsonString);
+        byte[] array;
+
+        try
+        {
+            var jsonString = JsonSerializer.Serialize(obj);
+            array = Encoding.UTF8.GetBytes(jsonString);
+        }
+        catch (InvalidOperationException)
+        {
+            // Do ignore. InvalidOperationException when try to serialize obj with Stream field
+            return int.MaxValue;
+        }
+        catch (NotSupportedException)
+        {
+            // Do ignore. Serialization and deserialization of 'System.Type' instances is not supported. Path: $.EntitySelector.Type.
+            return int.MaxValue;
+        }
+
         return array.Length;
     }
 }
