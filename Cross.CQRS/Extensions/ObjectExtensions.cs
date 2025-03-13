a94cdfd9e51c3be12f933b138423c38980cff08e
Denis Peshkov <denis.peshkov@outlook.com>
2025-03-13 15:31:39 +0200

    Added NoStopWatchMeasurementAttribute.

--- Cross.CQRS/Extensions/ObjectExtensions.cs
+++ Cross.CQRS/Extensions/ObjectExtensions.cs
@@ -4,8 +4,18 @@ public static class ObjectExtensions
 {
     public static int GetObjectSize(this object obj)
     {
-        var jsonString = JsonSerializer.Serialize(obj);
-        var array = Encoding.UTF8.GetBytes(jsonString);
+        var array = Array.Empty<byte>();
+
+        try
+        {
+            var jsonString = JsonSerializer.Serialize(obj);
+            array = Encoding.UTF8.GetBytes(jsonString);
+        }
+        catch (InvalidOperationException)
+        {
+            // do ignore InvalidOperationException when try to serialize obj with Stream field
+        }
+
         return array.Length;
     }
 }
