293b59cd9a4f5b7975f3b2d1a7282847b981f5cd
Denis Peshkov <denis.peshkov@outlook.com>
2026-03-24 13:16:24 +0200

    small fixes

---
+++ Cross.CQRS/AssemblyInfo.cs
@@ -0,0 +1,1 @@
+﻿[assembly: InternalsVisibleTo("Cross.CQRS.Tests")]
--- Cross.CQRS/GlobalUsings.cs
+++ Cross.CQRS/GlobalUsings.cs
@@ -1,3 +1,5 @@
+// Global using directives
+
 global using System;
 global using System.Collections.Concurrent;
 global using System.Collections.Generic;
@@ -5,6 +7,7 @@ global using System.Diagnostics;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Linq;
 global using System.Reflection;
+global using System.Runtime.CompilerServices;
 global using System.Security.Claims;
 global using System.Security.Cryptography;
 global using System.Text;
